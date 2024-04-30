e313d9803ff86e6e3f086a66e1b1f6691d1f23f4
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-04-30 19:19:12 -0300

    cambios

---
+++ TP5_GRUPO_1/ClassSQLconexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace TP5_GRUPO_1
+{
+    public class ClassSQLconexion
+    {
+        private String ruta = "Data Source=localhost\\sqlexpress;Initial Catalog = BDSucursales; Integrated Security = True";
+    }
+}
